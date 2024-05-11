db75484fe1b2391be8b2372d15897ce15643b94b
Mavelasse <46313778+Salondar@users.noreply.github.com>
2024-05-11 16:57:35 +0200

    Add operate multiply subtract divide and add functions

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function divide(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function operate(a, op, b) {
+    let result;
+    switch(op) {
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+    }
+    return result;
+}
